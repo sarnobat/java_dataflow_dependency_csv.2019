d09cb6b54dac26a3555425a0c209419ddd393c4c
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-20 16:39:44 -0800

    Minor changes (untested)

--- java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
+++ java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
@@ -2,29 +2,36 @@ package com.rohidekar;
 
 import java.io.*;
 
-/** @since 2018-12 */
+/** @since 2018-12 usage: -Dexec.args="/Users/ssarnobat/trash/myproj/target" */
 public class Main {
   public static void main(String[] args) {
-    BufferedReader br = null;
-    try {
-      br = new BufferedReader(new InputStreamReader(System.in));
-      String line;
-      while ((line = br.readLine()) != null) {
-        // log message
-        System.err.println("[DEBUG] current line is: " + line);
-        // program output
-        System.out.println(line);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (br != null) {
-        try {
-          br.close();
-        } catch (IOException e) {
-          e.printStackTrace();
+
+    if (args == null || args.length < 1) {
+      BufferedReader br = null;
+      try {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = br.readLine()) != null) {
+          // log message
+          System.err.println("[DEBUG] current line is: " + line);
+          // program output
+          System.out.println(line);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      } finally {
+        if (br != null) {
+          try {
+            br.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
         }
       }
+    } else {
+      String resource = args[0];
     }
+
+    System.err.println("Now use d3_helloworld_csv.git/singlefile_automated/ for visualization");
   }
 }
