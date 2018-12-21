62f9e949a383fe7dd3e19de62062816e71ca92c6
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-20 16:53:10 -0800

    Minor changes (untested)

--- java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
+++ java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
@@ -1,12 +1,27 @@
 package com.rohidekar;
 
 import java.io.*;
+import java.util.Collection;
+import java.util.LinkedList;
 
 /** @since 2018-12 usage: -Dexec.args="/Users/ssarnobat/trash/myproj/target" */
 public class Main {
   public static void main(String[] args) {
 
-    if (args == null || args.length < 1) {
+    Collection<String> dirsPathsContainingClasses = new LinkedList<String>();
+    {
+      if (args == null || args.length < 1) {
+      } else {
+        // note it may not be the immediate parent of the class files (obviously?)
+        String dirContainingClasses = args[0];
+        dirsPathsContainingClasses.add(dirContainingClasses);
+      }
+    }
+
+    // It's better to use STDIN for class files rather than class dirs so that
+    // we can add more selectively (including from a txt file)
+    Collection<String> classFilePaths = new LinkedList<String>();
+    {
       BufferedReader br = null;
       try {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,8 +29,7 @@ public class Main {
         while ((line = br.readLine()) != null) {
           // log message
           System.err.println("[DEBUG] current line is: " + line);
-          // program output
-          System.out.println(line);
+          classFilePaths.add(line);
         }
       } catch (IOException e) {
         e.printStackTrace();
@@ -28,8 +42,6 @@ public class Main {
           }
         }
       }
-    } else {
-      String resource = args[0];
     }
 
     System.err.println("Now use d3_helloworld_csv.git/singlefile_automated/ for visualization");
