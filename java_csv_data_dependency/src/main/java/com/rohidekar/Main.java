d89c623ba8d527306711397fd4d96ca0efd17985
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-27 17:25:48 -0800

    Minor changes (untested)

--- java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
+++ java_csv_data_dependency/src/main/java/com/rohidekar/Main.java
@@ -11,23 +11,30 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.Constant;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.LocalVariable;
+import org.apache.bcel.classfile.LocalVariableTable;
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.ACONST_NULL;
 import org.apache.bcel.generic.ALOAD;
+import org.apache.bcel.generic.ARETURN;
+import org.apache.bcel.generic.ASTORE;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.ConstantPushInstruction;
+import org.apache.bcel.generic.DUP;
 import org.apache.bcel.generic.GETSTATIC;
+import org.apache.bcel.generic.GOTO;
 import org.apache.bcel.generic.IFNONNULL;
-import org.apache.bcel.generic.INVOKEINTERFACE;
 import org.apache.bcel.generic.INVOKESPECIAL;
 import org.apache.bcel.generic.INVOKESTATIC;
 import org.apache.bcel.generic.INVOKEVIRTUAL;
 import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.generic.InstructionHandle;
+import org.apache.bcel.generic.LDC;
 import org.apache.bcel.generic.MethodGen;
+import org.apache.bcel.generic.NEW;
 import org.apache.bcel.generic.ObjectType;
 import org.apache.bcel.generic.PUTFIELD;
 import org.apache.bcel.generic.PUTSTATIC;
@@ -35,7 +42,6 @@ import org.apache.bcel.generic.RETURN;
 import org.apache.bcel.generic.Type;
 
 import gr.gousiosg.javacg.stat.ClassVisitor;
-import gr.gousiosg.javacg.stat.MethodVisitor;
 
 /**
  * 2018-12
@@ -107,15 +113,192 @@ public class Main {
         System.err.println("1) " + javaClass.getClassName());
         // Methods
         for (Method method : javaClass.getMethods()) {
-          System.err.println("2) " + javaClass.getClassName() + " :: " + method.getName());
-
-          MethodGen methodGen =
-              new MethodGen(
-                  method,
-                  javaClass.getClassName(),
-                  new ConstantPoolGen(javaClass.getConstantPool()));
+          ConstantPoolGen cpg = new ConstantPoolGen(javaClass.getConstantPool());
+          MethodGen methodGen = new MethodGen(method, javaClass.getClassName(), cpg);
+          LocalVariableTable symbolTable = methodGen.getMethod().getLocalVariableTable();
           System.err.println("2) " + javaClass.getClassName() + " :: " + method.getName() + "()");
-          visitMethod(methodGen, methodGen.getConstantPool());
+          // main bit
+          if (methodGen.getInstructionList() != null) {
+
+            Stack<String> stack = new Stack<String>();
+            for (InstructionHandle instructionHandle = methodGen.getInstructionList().getStart();
+                instructionHandle != null;
+                instructionHandle = instructionHandle.getNext()) {
+              Instruction anInstruction = instructionHandle.getInstruction();
+
+              if (anInstruction instanceof INVOKEVIRTUAL) {
+                //              System.out.println(
+                //                  "  3) INVOKEVIRTUAL\t"
+                //                      + ((INVOKEVIRTUAL) anInstruction).getReferenceType(constantPoolGen)
+                //                      + " :: "
+                //                      + ((INVOKEVIRTUAL) anInstruction).getMethodName(constantPoolGen)
+                //                      + "()");
+                //              anInstruction.accept(this);
+              } else if (anInstruction instanceof ConstantPushInstruction) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + " ConstantPushInstruction = "
+                        + ((ConstantPushInstruction) anInstruction).getValue());
+              } else if (anInstruction instanceof ACONST_NULL) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + " "
+                        + anInstruction);
+                //              anInstruction.accept(this);
+
+              } else if (anInstruction instanceof DUP) {
+                System.err.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() DUP (duplicate the value on top of the stack) "
+                        + stack.peek());
+                stack.push(stack.peek());
+              } else if (anInstruction instanceof GETSTATIC) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() GETSTATIC (get a static field value of a class, where the field is identified by field reference in the constant pool index): "
+                        + ((GETSTATIC) anInstruction).getFieldName(cpg));
+                //anInstruction.accept(this);
+              } else if (anInstruction instanceof PUTSTATIC) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() PUTSTATIC (set static field to value in a class, where the field is identified by a field reference index in constant pool): "
+                        + ((PUTSTATIC) anInstruction).getFieldName(cpg));
+              } else if (anInstruction instanceof NEW) {
+                System.err.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() NEW (create new object of type identified by class reference in constant pool index) "
+                        + anInstruction);
+              } else if (anInstruction instanceof ARETURN) {
+                System.err.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() ARETURN reference - "
+                        + anInstruction);
+                System.err.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() ARETURN reference - "
+                        + ((ARETURN) anInstruction).toString(javaClass.getConstantPool()));
+              } else if (anInstruction instanceof RETURN) {
+                System.err.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() RETURN void - "
+                        + anInstruction);
+              } else if (anInstruction instanceof IFNONNULL) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() "
+                        + anInstruction);
+                //anInstruction.accept(this);
+              } else if (anInstruction instanceof GOTO) {
+
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + " GOTO (goes to another instruction at branchoffset): "
+                        + anInstruction);
+              } else if (anInstruction instanceof ALOAD) {
+                int variableIndex = ((ALOAD) anInstruction).getIndex();
+                if (methodGen.getMethod().getLocalVariableTable() == null) {
+                  System.err.println(
+                      "  (unhandled) "
+                          + javaClass.getClassName()
+                          + "::"
+                          + method.getName()
+                          + " symbol table is null for "
+                          + methodGen.getMethod());
+                } else {
+                  LocalVariable variable =
+                      methodGen
+                          .getMethod()
+                          .getLocalVariableTable()
+                          .getLocalVariable(variableIndex, 0);
+                  String string = variable == null ? "null" : variable.getName();
+                  stack.push(string);
+                }
+              } else if (anInstruction instanceof ASTORE) {
+
+                if (methodGen.getMethod().getLocalVariableTable() == null) {
+                  System.err.println(
+                      "  (unhandled) "
+                          + javaClass.getClassName()
+                          + "::"
+                          + method.getName()
+                          + "() symbol table is null for "
+                          + methodGen.getMethod());
+                } else {
+                  String variableName =
+                      methodGen
+                          .getMethod()
+                          .getLocalVariableTable()
+                          .getLocalVariable(((ASTORE) anInstruction).getIndex())
+                          .getName();
+                  System.out.println(
+                      "Main.main() ASTORE (store a reference into a local variable #index): Declared and assigned variable:  "
+                          + variableName);
+                }
+              } else if (anInstruction instanceof INVOKESTATIC) {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() "
+                        + anInstruction
+                        + " INVOKESTATIC (invoke a static method and puts the result on the stack (might be void); the method is identified by method reference index in constant pool): ");
+              } else if (anInstruction instanceof INVOKESPECIAL) {
+              } else if (anInstruction instanceof LDC) {
+                stack.push(((LDC) anInstruction).getValue(cpg).toString());
+              } else if (anInstruction instanceof PUTFIELD) {
+                PUTFIELD p = (PUTFIELD) anInstruction;
+                String fieldNameBeingAssigned = p.getName(methodGen.getConstantPool());
+                while (!stack.isEmpty()) {
+                  System.out.println(
+                      "  3) " + fieldNameBeingAssigned + "\t--[depends on]--> " + stack.pop());
+                }
+              } else {
+                System.out.println(
+                    "  (unhandled) "
+                        + javaClass.getClassName()
+                        + "::"
+                        + method.getName()
+                        + "() "
+                        + anInstruction);
+                throw new RuntimeException(
+                    "Instruction to consider visiting: " + anInstruction.getClass());
+              }
+            }
+          }
 
           // fields
           Field[] fs = javaClass.getFields();
@@ -130,120 +313,5 @@ public class Main {
     }
 
     System.err.println("Now use d3_helloworld_csv.git/singlefile_automated/ for visualization");
-  }
-
-  /**
-   * This is the entry point to the magic / workhorse. Everything outside here is my algorithm. Most
-   * of this layer is using BECL's visitor.
-   *
-   * <p>I hate the visitor pattern, it's so object-oriented. We keep its class life time as small as
-   * possible
-   */
-  @Deprecated
-  private static class MyClassVisitor extends ClassVisitor {
-
-    private JavaClass classToVisit;
-
-    public MyClassVisitor(JavaClass classToVisit) {
-      super(classToVisit);
-      this.classToVisit = classToVisit;
-    }
-
-    @Deprecated
-    public void visitMethod(Method method) {
-      String className = classToVisit.getClassName();
-      ConstantPoolGen classConstants = new ConstantPoolGen(classToVisit.getConstantPool());
-      MethodGen methodGen = new MethodGen(method, className, classConstants);
-      System.err.println("2) " + className + " :: " + method.getName() + "()");
-      //new MyMethodVisitor(methodGen, classToVisit).start();
-    }
-
-    @Override
-    public void visitField(Field field) {
-      Type fieldType = field.getType();
-      if (fieldType instanceof ObjectType) {}
-    }
-  }
-
-  static void visitMethod(MethodGen methodGen, ConstantPoolGen constantPoolGen) {
-    // main bit
-    if (methodGen.getInstructionList() != null) {
-
-      Stack<String> stack = new Stack<String>();
-      System.out.println("2) " + methodGen.getClassName() + " :: " + methodGen.getName());
-      for (InstructionHandle instructionHandle = methodGen.getInstructionList().getStart();
-          instructionHandle != null;
-          instructionHandle = instructionHandle.getNext()) {
-        Instruction anInstruction = instructionHandle.getInstruction();
-
-        if (anInstruction instanceof INVOKEVIRTUAL) {
-          //              System.out.println(
-          //                  "  3) INVOKEVIRTUAL\t"
-          //                      + ((INVOKEVIRTUAL) anInstruction).getReferenceType(constantPoolGen)
-          //                      + " :: "
-          //                      + ((INVOKEVIRTUAL) anInstruction).getMethodName(constantPoolGen)
-          //                      + "()");
-          //              anInstruction.accept(this);
-        } else if (anInstruction instanceof ConstantPushInstruction) {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() ConstantPushInstruction = "
-                  + ((ConstantPushInstruction) anInstruction).getValue());
-        } else if (anInstruction instanceof ACONST_NULL) {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //              anInstruction.accept(this);
-
-        } else if (anInstruction instanceof RETURN) {
-          //((RETURN)anInstruction).
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //              throw new RuntimeException("RETURN");
-        } else if (anInstruction instanceof PUTSTATIC) {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //  anInstruction.accept(this);
-        } else if (anInstruction instanceof IFNONNULL) {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //anInstruction.accept(this);
-        } else if (anInstruction instanceof GETSTATIC) {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //anInstruction.accept(this);
-        } else if (anInstruction instanceof ALOAD) {
-          int variableIndex = ((ALOAD) anInstruction).getIndex();
-          if (methodGen.getMethod().getLocalVariableTable() == null) {
-            System.err.println(
-                "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() symbol table is null for "
-                    + methodGen.getMethod());
-          } else {
-            LocalVariable variable =
-                methodGen.getMethod().getLocalVariableTable().getLocalVariable(variableIndex, 0);
-            String string = variable == null ? "null" : variable.getName();
-            stack.push(string);
-          }
-        } else if (anInstruction instanceof INVOKESPECIAL) {
-          // e.g. java.lang.Object :: <init>()
-          //              System.err.println(
-          //                  "  3) INVOKESPECIAL\t"
-          //                      + ((INVOKESPECIAL) anInstruction).getReferenceType(constantsPool)
-          //                      + " :: "
-          //                      + ((INVOKESPECIAL) anInstruction).getMethodName(constantsPool)
-          //                      + "()");
-        } else if (anInstruction instanceof PUTFIELD) {
-          PUTFIELD p = (PUTFIELD) anInstruction;
-          String fieldNameBeingAssigned = p.getName(constantPoolGen);
-          while (!stack.isEmpty()) {
-            System.out.println(
-                "  3) " + fieldNameBeingAssigned + " --[depends on]--> " + stack.pop());
-          }
-        } else {
-          System.out.println(
-              "(unhandled) Main.MyClassVisitor.MyMethodVisitor.visitMethod() " + anInstruction);
-          //              throw new RuntimeException(
-          //                  "Instruction to consider visiting: " + anInstruction.getClass());
-        }
-      }
-    }
   }
 }
