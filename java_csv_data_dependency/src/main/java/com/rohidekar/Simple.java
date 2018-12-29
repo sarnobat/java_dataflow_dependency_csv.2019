c6fc7f28ca57abc2c704b8f57a76c8c8248068d8
Sridhar Sarnobat <ssarnobat@control4.com>
2018-12-29 15:37:54 -0800

    Minor changes (untested)

---
+++ java_csv_data_dependency/src/main/java/com/rohidekar/Simple.java
@@ -0,0 +1,14 @@
+package com.rohidekar;
+
+/**
+ * This class is just to help me figure out what instruction sequences denote what high level
+ * construct
+ */
+public class Simple {
+
+  public static void main(String[] args) {
+    voidMethod("foo", "bar");
+  }
+
+  private static void voidMethod(String param1, String param2) {}
+}
