2cf4c099f1eb502d4391cf363098f4cd2cf82bfd
Sridhar Sarnobat <ss401533@gmail.com>
2023-06-08 19:20:01 -0700

    Minor changes (untested)

---
+++ java_csv_data_dependency/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#----------------------------------------------------------------------------
+# DESCRIPTION		
+# DATE				[:VIM_EVAL:]strftime('%Y-%m-%d')[:END_EVAL:]
+# AUTHOR			ss401533@gmail.com                                           
+#----------------------------------------------------------------------------
+
+#set -e
+set -o errexit
+
+test $# -gt 0 && echo "args given" || echo "no args"
+# TODO: string comparison check (both ways)
+
+cat <<EOF | batcat --plain --paging=never --language sh --theme TwoDark
+mvn assembly:single
+
+find $PWD -maxdepth 50 -type f -iname "**class" | java -classpath /Volumes/git/github/java_dataflow_dependency_csv/java_csv_data_dependency/target/java_csv_data_dependency-1.0-SNAPSHOT-jar-with-dependencies.jar  com.rohidekar.Main
+EOF
