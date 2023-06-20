02a18ca09f5c86b8a07e08d3b0c1cce1c6039d05
Sridhar Sarnobat <ss401533@gmail.com>
2023-06-19 21:13:21 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-
+cat <<EOF | batcat --plain --paging=never --language sh --theme TwoDark
 #find /Users/ssarnobat/webservices/cmp/authentication-services/target/classes/ -iname "*class" \
 #	|  /Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -classpath /Users/ssarnobat/github/java_data_dependency_csv/java_csv_data_dependency/target/classes:/Users/ssarnobat/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:/Users/ssarnobat/.m2/repository/org/apache/bcel/bcel/6.0/bcel-6.0.jar:/Users/ssarnobat/.m2/repository/commons-collections/commons-collections/3.2.2/commons-collections-3.2.2.jar:/Users/ssarnobat/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar com.rohidekar.Main
 	
@@ -11,5 +11,18 @@ cd ~/github/java_dataflow_dependency_csv/java_csv_data_dependency/ && find $PWD 
 # 2021
 find $PWD -maxdepth 50  -iname "**class" | java -jar /Volumes/Homes/srsarnob/github/java_dataflow_dependency_csv/java_csv_data_dependency/target/java_csv_data_dependency-1.0-SNAPSHOT-jar-with-dependencies.jar  2>/dev/null  | perl -pe 's{\s*...depends on( variable)?....\s*}{","}g' | perl -pe 's{(^.*$)}{"$1"}g' | sort | tee /tmp/variable_dependencies.csv
 
+##
+## 2023
+##
+
+#
 # Now use d3_helloworld_csv.git/singlefile_automated/ for visualization
+#
 cd /Users/srsarnob/github/d3_csv/singlefile_automated && cat /tmp/variable_dependencies.csv  | sh csv2d3.sh  | tee /tmp/index.html && popd
+
+#
+# awk
+#
+javap -v /Volumes/git/github/java_dataflow_dependency_csv/java_csv_data_dependency/target/classes/com/rohidekar/analyzed/Simple* | tee /tmp/class.txt | awk -f ~/bin/awk_java_instructions.awk | grep -v unimple
+
+EOF
